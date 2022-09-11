d11a7ce4715950371e252991dd62efc23856c434
ocbobo <ocbobo01@gmail.com>
2022-09-11 04:11:58 -0700

    Print 0-9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
-#include <unistd.h>
+
 /**
-* main - Entry point
-* Description: prints numbers
-* Return: Always 0 (success)
+* main - Prints a series of numbers with commas
+*
+* Return: Always (Success);
 */
 int main(void)
 {
-	int i;
+	int c;
 
-	for (i = '0'; i <= '9' ; i++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		putchar(i);
+		putchar(c);
 	
-		if (i != '9')
+		if (c != '9')
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
+	
 	putchar('\n');
+
 	return (0);
 }
