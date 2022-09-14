bdbd125abc36d3287416087719118159159cec24
ocbobo <ocbobo01@gmail.com>
2022-09-14 08:44:08 -0700

    prints the 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,7 +6,7 @@
 void times_table(void)
 {
 	int num, mult, prod;
-
+			
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
@@ -15,16 +15,18 @@ void times_table(void)
 		{
 			_putchar(',');
 			_putchar(' ');
-
+			
 			prod = num * mult;
-
+			
 			if (prod <= 9)
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + '0');
-			
+		
 			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
 	}
 }
+
+			
