283542e63faa787fd7f48d2d5dedf765103c6af5
ocbobo <ocbobo01@gmail.com>
2022-09-17 23:10:47 -0700

    prints 0-9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,15 +2,17 @@
 #include "main.h"
 
 /**
- * print_numbers - prints numbers between 0 to 9.
- * Return: no return.
+ * print_numbers - function that prints all numbers
+ * 0 to 9
+ *
+ * Return: returns nothing
  */
 
 void print_numbers(void)
 {
 	int n;
 	
-	for (n = 48; ch < 58; ch++)
+	for (n = 48; ch < 58; n++)
 	{		
   	_putchar(n);
 	}
