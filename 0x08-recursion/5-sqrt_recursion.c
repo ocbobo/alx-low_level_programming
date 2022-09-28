2121264d2eb7f9cb075f9a00fce9744c0ea4bcd0
ocbobo <ocbobo01@gmail.com>
2022-09-28 03:36:46 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -15,3 +15,16 @@ int sqrt_check(int g, int c)
 		return (-1);
 	return (sqrt_check(g + 1, c));
 }
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: integer to find sqrt of
+ *
+ * Return: natural square root or -1
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	return (sqrt_check(1, n));
+}
