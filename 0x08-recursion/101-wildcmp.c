f09e5dae8d5ddf5be483b485f285f543afa9cae1
ocbobo <ocbobo01@gmail.com>
2022-09-28 04:12:28 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * wildcard - compares two strings and return 1 if identical
+ * wildcmp - compares two strings and return 1 if identical
  * @s1: string to be checked
  * @s2: pattern to be used
  *
@@ -14,6 +14,6 @@ int wildcmp(char *s1, char *s2)
 	if (*s2 == *s1)
 		return (*s1 !== '\0' && wildcmp(s1 + 1, s2 + 1));
 	if (*s2 == '*')
-		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
+		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)))
 	return (0);
 }
