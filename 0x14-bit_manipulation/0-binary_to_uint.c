37396d9de20780fe6d75b0cc54aa091ddb0f4d67
ocbobo <ocbobo01@gmail.com>
2022-10-28 01:31:06 -0700

    TASK

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,32 +1,37 @@
-#include <stddef.h>
+#include "main.h"
+
 /**
- * binary_to_uint - converts a bit string to unsigned int
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
  *
- * @b: string to convert
- *
- * Return: decimal unsigned int value
+ * Return: unsigned int.
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int n;
+	unsigned int ui;
+	int len, base_two;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
-	for (n = 0; *b != 0; b++)
+
+	ui = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		if (*b == '0')
-		{
-			n = n << 1;
-		}
-		else if (*b == '1')
-		{
-			n = n << 1;
-			n++;
-		}
-		else
+		if (b[len] != '0' && b[len] != '1')
 		{
 			return (0);
 		}
+
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
 	}
-	return (n);
+
+	return (ui);
 }
